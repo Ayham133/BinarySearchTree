dd45223dbe44ae887c9681b77188ce0684c5c537
= <=>
2025-12-08 00:07:52 +0200

    Iam on Fire, just tested the delete method and it works very good for now

--- src/java/main/Main.java
+++ src/java/main/Main.java
@@ -6,37 +6,13 @@ public class Main {
     public static void main(String[] args) {
         AVLTree tree = new AVLTree();
 
-        tree.insert(10);
-        tree.insert(20);
-        tree.insert(50);
         tree.insert(60);
+        tree.insert(50);
+        tree.insert(20);
+        tree.insert(10);
 
         tree.preOrderPrint();
-        System.out.println();
 
-        tree.delete(10);
-
-        // bst.printLeftChildOnly();
-        //
-        // bst.printRightChildOnly();
-        //
-        // bst.printLeaf();
-        // System.out.println(bst.countLeaf());
-        //
-        // bst.printParent();
-        // System.out.println(bst.countParent());
-        //
-        // bst.preOrderPrint();
-        // System.out.println();
-        //
-        // System.out.println("BST Height is: " + bst.height());
-        //
-        // System.out.println("Search for 14: " + bst.search(14));
-        //
-        // System.out.println("The Minimum Value in this BST is: " +
-        // bst.minimumElement());
-        //
-        // System.out.println("The Maximum Value in this BST is: " +
-        // bst.maximumElement());
+        tree.delete(60);
     }
 }
